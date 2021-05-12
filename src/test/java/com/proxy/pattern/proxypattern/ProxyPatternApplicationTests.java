17edd68c25524b95388975321fbdec44a2323ec6
marcelo.maximiano <marcelomax159@gmail.com>
2021-05-11 21:32:25 -0300

    first commit

---
+++ src/test/java/com/proxy/pattern/proxypattern/ProxyPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proxy.pattern.proxypattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
